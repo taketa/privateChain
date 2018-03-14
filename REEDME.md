247892f5d65ed5c51b76fe94d4e46ef2fd303bde
taketa <853211b@gmail.com>
2018-03-14 15:13:18 +0200

    add comments

--- REEDME.md
+++ REEDME.md
@@ -1,3 +1,22 @@
-1) geth init privateChain.json
-2) geth --networkid 111 --bootnodes enode://6ccd34b8f01c3ae3240eee1467dbf97217c85b866c2756e8eb1ddd67377f4c6209afff709e908b6863e44cf48b80ba215894da3931aa60f1d5a1bcb9fac239a8@192.168.88.62:30303 --rpc console
+Создание приватной блокчейн-сети:
+1) Создаем несколько аккаунтов:
+geth account new
+2) создание genesis.json файла:
+puppeth
+3) инициализация genesis файла:
+geth init genesis.json
+    -если при создании возникает ошибка:
+    удаляем существующую базу- geth removedb
+    -если не помогает удаляем папку /home/taketa/.ethereum/geth/chaindata
+4) запускаем ноду:
+geth --networkid 123 --rpc console
+5) копируем адрес ноды и подставляем в него свой ip (пример: enode://6ccd34b8f01c3ae3240eee1467dbf97217c85b866c2756e8eb1ddd67377f4c6209afff709e908b6863e44cf48b80ba215894da3931aa60f1d5a1bcb9fac239a8@192.168.88.62:30303)
 
+Подключение пиров к ноде:
+1) Инициализируем genesis.json:
+geth init genesis.json
+2) подключаемся к ноде:
+geth --networkid 123 --bootnodes enode://6ccd34b8f01c3ae3240eee1467dbf97217c85b866c2756e8eb1ddd67377f4c6209afff709e908b6863e44cf48b80ba215894da3931aa60f1d5a1bcb9fac239a8@192.168.88.62:30303 console
+
+Проверка подключились ли пиры:
+admin.peers
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import(
 	"log"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
-	"ether"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"html/template"
 	"contracts"
@@ -14,7 +13,9 @@ import(
 	"strings"
 	"time"
 	"github.com/ethereum/go-ethereum/rpc"
-
+	"os"
+	"io/ioutil"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 
@@ -44,33 +45,30 @@ type User struct {
 
 func init(){
 	tpl = template.Must(template.ParseGlob("templates/*"))
+	//accounts storage
 	accs = make(map[string]*bind.TransactOpts)
-	TransOpt=ether.GenTransactOpts("accounts")
+	//generate transaction opts. Collection of authorization data required to create a valid Ethereum transaction.
+	TransOpt=GenTransactOpts("accounts")
+	//creating client that represents a connection to an RPC server.
 	rpcClient,err := rpc.Dial("http://127.0.0.1:8545")
 	if err!=nil{
 		log.Fatalln("Dial 49: ",err)
 	}
+	//NewClient creates a client that uses the given RPC client.
 	conn:=ethclient.NewClient(rpcClient)
-
+	//creating and deploying smart contract
 	contractAddr,_, contract,err = contracts.DeploySimpleStorage(TransOpt[0],conn)
 	fmt.Println("ContractAddress: ",common.ToHex(contractAddr[:]))
 	if err!=nil{
-		//bal,_:=conn.PendingBalanceAt(context.TODO(),TransOpt[0].From)
-		//fmt.Printf("Ballance of %v: %v \n",common.ToHex(TransOpt[0].From[:]),bal)
 		log.Fatalln("Contract deploy 53: ",err)
 	}
+	//The approximate time for mining block
 	time.Sleep(time.Second*10)
-	vis, _ := contract.GetVisa(&bind.CallOpts{
-		From:TransOpt[0].From,
-	})
 
-
-	fmt.Println("Visa: ",vis)
-	fmt.Println(contract)
 }
 
-
 func main() {
+	//run server
 	mux := httprouter.New()
 	mux.GET("/", index)
 	mux.POST("/userData", userData)
@@ -85,13 +83,10 @@ func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 }
 
 func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	vis, _ := contract.GetVisa(&bind.CallOpts{
-		From:TransOpt[0].From,
-	})
-	fmt.Println("VISA is: ",vis)
 	req.ParseForm()
+	//check that request comes from /userChange
 	if len(req.Form)!=1 && len(req.Form)!=0{
-
+		//checks for nationality is set
 		if req.Form["nationality"][0]!=dataToTemplate.Nationality {
 			fmt.Println("NAtionality!!!!")
 			contract.SetNationality(&bind.TransactOpts{
@@ -102,7 +97,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 			}, req.Form["nationality"][0])
 			fmt.Println("Mining Nationality\n")
 		}
-		//time.Sleep(time.Second*15)
+		//checks for visa is set
 		if req.Form["visa"][0]!=dataToTemplate.Visa {
 			fmt.Println("VISA!!!!")
 			contract.SetVisa(&bind.TransactOpts{
@@ -114,7 +109,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 			fmt.Println("Mining Visa\n")
 
 		}
-
+		//checks for age is set
 		if req.Form["age"][0]!=dataToTemplate.Age{
 			contract.SetAge(&bind.TransactOpts{
 				From:     account.From,
@@ -124,6 +119,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 			}, req.Form["age"][0])
 			fmt.Println("Mining Age\n")
 		}
+		//checks for speaks is set
 		if req.Form["speaks"][0]!=dataToTemplate.Speaks{
 			contract.SetSpeaks(&bind.TransactOpts{
 				From:     account.From,
@@ -133,6 +129,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 			}, req.Form["speaks"][0])
 			fmt.Println("Mining Speaks\n")
 		}
+		//checks for medConditions is set
 		if !testEq(strings.Split(req.Form["medCondition"][0]," "),dataToTemplate.MedCondition)  {
 			contract.SetMedCondition(&bind.TransactOpts{
 				From:     account.From,
@@ -142,6 +139,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 			}, req.Form["medCondition"][0])
 			fmt.Println("Mining MedicalConditions\n")
 		}
+		//checks for medMedication is set
 		if !testEq(strings.Split(req.Form["medMedication"][0]," "),dataToTemplate.MedMedications) {
 			contract.SetMedMedication(&bind.TransactOpts{
 				From:     account.From,
@@ -153,6 +151,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 		}
 
 	}else{
+		//request comes from /index
 		var data string
 		if len(req.Form)==0{
 			data=dataToTemplate.Name
@@ -163,20 +162,16 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 		if data=="John Doe"{
 			account=TransOpt[0]
 		}else{
-			inputHex := ether.StringToHex(data)
+			inputHex := StringToHex(data)
 			account=getAddress(inputHex)
-
 		}
-
-
 	}
-
-
 
 	//Get values from contract
 	name, _ := contract.GetName(&bind.CallOpts{
 		From:account.From,
 	})
+	//checks that user is new
 	if name ==""{
 		contract.SetName(&bind.TransactOpts{
 			From:     account.From,
@@ -208,13 +203,7 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 		From:account.From,
 	})
 	medMedications:=strings.Split(medMedicationsStr," ")
-
-
-
-
-
-
-
+	//fill in User fields   for paste to template
 	dataToTemplate=User{name, visa,
 		nationality,age,
 		speaks,medCondition,
@@ -224,19 +213,16 @@ func userData(w http.ResponseWriter, req *http.Request, params httprouter.Params
 	HandleError(w, err)
 
 }
-
 func userChange(w http.ResponseWriter, req *http.Request, params httprouter.Params){
 	err := tpl.ExecuteTemplate(w, "userChange.gohtml", dataToTemplate)
 	HandleError(w, err)
 }
-
 func HandleError(w http.ResponseWriter, err error) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Fatalln("Handle func error 212: ",err)
 	}
 }
-
 //getting address for hex string
 func getAddress(str string) (*bind.TransactOpts){
 	if _,ok:=accs[str];ok{
@@ -272,6 +258,36 @@ func testEq(a, b []string) bool {
 	return true
 }
 
+
+
+func StringToHex(str string) (string){
+	bs := []byte(str)
+	//Keccak256 hash
+	sha3:= [32]byte(crypto.Keccak256Hash(bs[:]))
+	return common.Bytes2Hex(sha3[:])
+}
+
+//generate transaction opts for private chain
+func GenTransactOpts(src string) ([]*bind.TransactOpts){
+	var TransOpt []*bind.TransactOpts
+	files, err := ioutil.ReadDir(src)
+	if err!=nil{
+		log.Fatalln("GenTransactOpts() files: ",err)
+	}
+	//loads key files
+	for _,fInfo:=range files{
+		file,err:=os.Open(src+"/"+fInfo.Name())
+		if err!=nil{
+			log.Fatalln("GenTransactOpts() file: ",err)
+		}
+		trOpt,err:=bind.NewTransactor(file, "1")
+		if err!=nil{
+			log.Fatalln("GenTransactOpts() trOpt: ",err)
+		}
+		TransOpt=append(TransOpt,trOpt)
+	}
+	return TransOpt
+}
 //key:="b2cb8c8c402ad1fd6bcdb401db3603094e9c0b3f77e90238271d328aa54ec1f7"
 //keyBs:=common.Hex2Bytes(key)
 //fmt.Println("keyBs===",keyBs)
